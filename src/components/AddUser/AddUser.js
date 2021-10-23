357afe9aa5686ee470c64f60a91a9c8c56ec81b6
rasel <russelroy15@gmail.com>
2021-10-24 03:59:39 +0600

    WIP on react node mongo starter

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,16 +1,31 @@
 import React, { useRef } from "react";
 
 const AddUser = () => {
-    const nameRef = useRef();
-    const emailRef = useRef();
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const handleAddUser = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-          <h2>Please Add an user</h2>
-          <form onSubmit={ }>
-              <input type="text" ref={nameRef} name="text" id="" placeholder="Your name" />
-              <input type="email" ref={emailRef} name="email" id="" placeholder="Your Email" />
-              <input type="submit" value="Submit" />
-          </form>
+      <h2>Please Add an user</h2>
+      <form onSubmit={handleAddUser}>
+        <input
+          type="text"
+          ref={nameRef}
+          name="text"
+          id=""
+          placeholder="Your name"
+        />
+        <input
+          type="email"
+          ref={emailRef}
+          name="email"
+          id=""
+          placeholder="Your Email"
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
