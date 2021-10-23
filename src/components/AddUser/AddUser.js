71d430175c9387f0d47ed99894b21d8a11a8f673
rasel <russelroy15@gmail.com>
2021-10-24 03:54:35 +0600

    WIP on react node mongo starter

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const AddUser = () => {
-    return (
-        <div>
-            <h2>This is Add User</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Please Add an user</h2>
+    </div>
+  );
 };
 
 export default AddUser;
