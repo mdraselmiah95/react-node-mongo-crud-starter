c9c69de5911a3cb3dddf7c71706b562d07eb4a57
rasel <russelroy15@gmail.com>
2021-10-24 07:03:09 +0600

    WIP on react node mongo starter

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -8,13 +8,20 @@ const Users = () => {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
+  //DELETE AN USER
+
+  const handleDeleteUser = (id) => {};
   return (
     <div>
       <h2>Users Available: {users.length}</h2>
       {users.map((user) => (
         <div key={user._id}>
-          <h3>{user.name}</h3>
-          <h5>{user.email}</h5>
+          <h3>
+            {user.name} :: {user.email}
+            <button>Update</button>
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
+          </h3>
         </div>
       ))}
     </div>
