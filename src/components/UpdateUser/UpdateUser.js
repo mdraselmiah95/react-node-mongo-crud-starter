095d23b08701ef565062c69f56c86067d2e83bca
rasel <russelroy15@gmail.com>
2021-10-24 07:38:38 +0600

    WIP on react node mongo starter

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const UpdateUser = () => {
-    return (
-        <div>
-            <h2>This is Update User</h2>
-        </div>
-    );
+  const { id } = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const url = `http://localhost:5000/users/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>Update {user.name}</h2>
+    </div>
+  );
 };
 
 export default UpdateUser;
