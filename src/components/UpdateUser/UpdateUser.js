69e70ab425ecb74194b2e699d302eb8b7159ed51
rasel <russelroy15@gmail.com>
2021-10-25 07:59:14 +0600

    WIP on react node mongo crud project

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -16,11 +16,15 @@ const UpdateUser = () => {
   //Update user
 
   const handleNameChange = (e) => {
-    const updateName = e.target.value;
-    const updatedUser = { name: updateName, email: user.email };
+    const updatedName = e.target.value;
+    const updatedUser = { name: updatedName, email: user.email };
     setUser(updatedUser);
   };
-  const handleEmailChange = (e) => {};
+  const handleEmailChange = (e) => {
+    const updatedEmail = e.target.value;
+    const updatedUser = { name: user.name, email: updatedEmail };
+    setUser(updatedUser);
+  };
 
   const handleUpdateUser = (e) => {
     e.preventDefault();
